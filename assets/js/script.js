f55b76ac704d3de8f043ae8c9a48daef0a767894
Yasas Bavindhu Withana <yazazb@gmail.com>
2020-07-29 11:59:36 +0530

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// alert("hello");
+
+// porfolio code
+
+// scrolltotopbutton
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > outerHeight ||
+    document.documentElement.scrollTop > outerHeight
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
